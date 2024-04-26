78f9e6751f765a5b6ba64e25d398bd1ce5af7a24
Alperen Yüksel <115918545+UrbanAlc@users.noreply.github.com>
2024-04-26 17:13:47 +0300

    Update flaskapi.py
    xxd

--- final-saga/kubernetes-flask-mysql/flaskapi.py
+++ final-saga/kubernetes-flask-mysql/flaskapi.py
@@ -19,7 +19,7 @@ mysql.init_app(app)
 @app.route("/")
 def index():
     """Function to test the functionality of the API"""
-    return "AYİ FİRST PİPELİNE XDDXDXDXD ASDASFASDAMSADqweqwez"
+    return "phind gptyi sikti götünden.com"
 
 @app.route('/health')
 def health_check():
