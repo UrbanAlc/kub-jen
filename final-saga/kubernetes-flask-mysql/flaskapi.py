06a87f97bb42cffb60e9d9ab20662cff43403996
Alperen Yüksel <115918545+UrbanAlc@users.noreply.github.com>
2024-04-28 12:14:13 +0300

    Update flaskapi.py
    build final

--- final-saga/kubernetes-flask-mysql/flaskapi.py
+++ final-saga/kubernetes-flask-mysql/flaskapi.py
@@ -19,7 +19,7 @@ mysql.init_app(app)
 @app.route("/")
 def index():
     """Function to test the functionality of the API"""
-    return "BABALAR SOZUNU TUTAR"
+    return "KPLUS Automation SYSTEM Build by AY"
 
 @app.route('/health')
 def health_check():
